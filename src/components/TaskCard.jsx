ffe6a943ac6b6e26138e509ed77d8c98780ce136
Abigael Salome Amere <abigail-salome>
2025-07-23 11:07:54 +0300

    rendered a list of tasks

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,44 @@
+
+import EditTask from "./EditTask";
+import RemoveTask from "./RemoveTask";
+import "./TaskCard.css";
+
+const TaskCard = ({ tasks, setTasks }) => {
+  const toggleComplete = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li key={task.id} className="task-item">
+            <button
+              className="circle-btn"
+              onClick={() => toggleComplete(task.id)}
+            >
+            </button>
+
+            <p className={task.completed ? "struck-text" : ""}>{task.title}</p>
+
+            {/* Optional details */}
+            {/* <p>{task.description}</p>
+            <p>Priority: {task.priority}</p>
+            <p>Completed: {task.completed ? "yes" : "no"}</p>
+            <p>Due: {task.dueDate}</p> */}
+
+            <div className="actions">
+              <EditTask tasks={tasks} setTasks={setTasks} taskId={task.id} />
+              <RemoveTask tasks={tasks} setTasks={setTasks} taskId={task.id} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskCard;
